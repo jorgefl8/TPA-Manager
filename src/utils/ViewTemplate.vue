3fa11bc697ae5c848233dc6c0b66e1702c693239
Jorge Florentino <jorgeflorentinomarbe@gmail.com>
2024-06-04 23:25:50 +0200

    feat: added a View to utils that follows the design of a view, due to create new View

---
+++ src/utils/ViewTemplate.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted, onUnmounted, } from 'vue'
+import Toast from 'primevue/toast';
+import { useToast } from "primevue/usetoast";
+import NavMenu from '@/components/NavMenu.vue';
+import Divider from 'primevue/divider';
+import { bluejayInfraStore } from '@/stores/bluejayInfra';
+
+const bluejayInfra = bluejayInfraStore();
+const toast = useToast();
+const isMobile = ref(window.innerWidth <= 768);
+const updateIsMobile = () => {
+    isMobile.value = window.innerWidth <= 768;
+};
+onMounted(() => {
+    window.addEventListener('resize', updateIsMobile);
+});
+onUnmounted(() => {
+    window.removeEventListener('resize', updateIsMobile);
+});
+
+//THIS VIEW FOLLOWS THE DESIGN OF TPA MANAGER FOR CREATING A NEW VIEW
+
+</script>
+<template>
+    <div style="display: grid; justify-items: center;">
+        <div class="card ">
+            <NavMenu />
+            <Divider layout="horizontal" />
+            <Toast ref="toast" :position="isMobile ? 'bottom-left' : 'bottom-right'" :baseZIndex="10000" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
